f29ac1e4d6a3c78073aa911e50e5ebd387fe8465
jrogan993 <jrogan993@gmail.com>
2017-05-16 17:43:40 +0100

    Add files via upload

---
+++ chapter2ex1.c
@@ -0,0 +1,8 @@
+/* Prints a message on screen */
+#include <stdio.h>
+main()
+{
+    printf("Just one small step for coders. One giant leap for");
+    printf(" programmers!\n");
+    return 0;
+}
---
+++ chapter4ex1.c
@@ -0,0 +1,14 @@
+// Chapter 4 example 1 -- escapeSequences.c
+
+#include <stdio.h>
+main()
+{
+	/* These three lines show you how to use the most popular escape 
+	 sequences in C */
+	printf("Column A\tColumn B\tColumn C");
+	printf("\nMy Computer\'s Beep Sounds Like  This: \a! \n");
+	printf("\"Letz\bs fix that typo and then show a backslash 	");
+	printf("character \\\" she said\n");
+	
+	return 0;
+}
